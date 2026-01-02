210e88add1ae57acbc205d76aca7a20d25b0cef0
xiaomu <jinyuan_mu@163.com>
2026-01-02 23:05:16 +0800

    complete week2 exercise

--- week2/rdiff/src/main.rs
+++ week2/rdiff/src/main.rs
@@ -1,37 +1,67 @@
 use grid::Grid; // For lcs()
 use std::env;
-use std::fs::File; // For read_file_lines()
+use std::fs::File;
+// For read_file_lines()
 use std::io::{self, BufRead}; // For read_file_lines()
 use std::process;
-
+use std::cmp::max;
 pub mod grid;
 
 /// Reads the file at the supplied path, and returns a vector of strings.
-#[allow(unused)] // TODO: delete this line when you implement this function
 fn read_file_lines(filename: &String) -> Result<Vec<String>, io::Error> {
-    unimplemented!();
-    // Be sure to delete the #[allow(unused)] line above
+    let file = File::open(filename)?;
+    let mut result = Vec::new();
+
+    for line in io::BufReader::new(file).lines() {
+        let line_str = line?;
+        result.push(line_str);
+    }
+
+    Ok(result)
 }
 
-#[allow(unused)] // TODO: delete this line when you implement this function
 fn lcs(seq1: &Vec<String>, seq2: &Vec<String>) -> Grid {
     // Note: Feel free to use unwrap() in this code, as long as you're basically certain it'll
     // never happen. Conceptually, unwrap() is justified here, because there's not really any error
     // condition you're watching out for (i.e. as long as your code is written correctly, nothing
     // external can go wrong that we would want to handle in higher-level functions). The unwrap()
     // calls act like having asserts in C code, i.e. as guards against programming error.
-    unimplemented!();
-    // Be sure to delete the #[allow(unused)] line above
+    let m = seq1.len();
+    let n = seq2.len();
+    let mut C = Grid::new(m + 1, n + 1);
+    for i in 0..m {
+        for j in 0..n {
+            if seq1[i] == seq2[j] {
+                let val = C.get(i, j).unwrap() + 1usize;
+                C.set(i+1, j+1, val);
+            } else {
+                let max_val = max(
+                    C.get(i + 1, j).unwrap(),
+                    C.get(i, j + 1).unwrap(),
+                );
+                C.set(i + 1, j + 1, max_val);
+            }
+        }
+    }
+    C
 }
 
-#[allow(unused)] // TODO: delete this line when you implement this function
 fn print_diff(lcs_table: &Grid, lines1: &Vec<String>, lines2: &Vec<String>, i: usize, j: usize) {
-    unimplemented!();
-    // Be sure to delete the #[allow(unused)] line above
+    if i > 0 && j > 0 && lines1[i-1] == lines2[j-1] {
+        print_diff(lcs_table, lines1, lines2, i -1, j - 1);
+        println!("  {}", lines1[i-1]);
+    } else if j > 0 && (i == 0 || lcs_table.get(i, j - 1).unwrap() >= lcs_table.get(i - 1, j).unwrap()) {
+        print_diff(lcs_table, lines1, lines2, i, j - 1);
+        println!("+ {}", lines2[j - 1]);
+    } else if i > 0 && (j == 0 || lcs_table.get(i, j - 1).unwrap() < lcs_table.get(i - 1, j).unwrap()) {
+        print_diff(lcs_table, lines1, lines2, i - 1, j);
+        println!("- {}", lines1[i - 1]);
+    } else {
+
+    }
 }
 
-#[allow(unused)] // TODO: delete this line when you implement this function
-fn main() {
+fn main() -> Result<(), std::io::Error>{
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         println!("Too few arguments.");
@@ -40,8 +70,12 @@ fn main() {
     let filename1 = &args[1];
     let filename2 = &args[2];
 
-    unimplemented!();
-    // Be sure to delete the #[allow(unused)] line above
+    let lines1 = read_file_lines(filename1)?;
+    let lines2 = read_file_lines(filename2)?;
+
+    let grid = lcs(&lines1, &lines2);
+    print_diff(&grid, &lines1, &lines2, lines1.len(), lines2.len());
+    Ok(())
 }
 
 #[cfg(test)]
--- week2/rwc/src/main.rs
+++ week2/rwc/src/main.rs
@@ -1,12 +1,65 @@
 use std::env;
+use std::io;
+use std::fs::File;
+use std::io::BufRead;
 use std::process;
 
-fn main() {
+pub struct WordCount {
+    words:usize,
+    lines:usize,
+    bytes:usize,
+}
+
+impl WordCount {
+    pub fn new(words_cnt:usize, lines_cnt:usize, bytes_cnt: usize) -> WordCount{
+        WordCount {
+            words : words_cnt,
+            lines : lines_cnt,
+            bytes : bytes_cnt,
+        }
+    }
+
+    pub fn display(&self) {
+        println!("words_cnt:{}, lines_cnt:{}, bytes_cnt:{}", self.words, self.lines, self.bytes);
+    }
+}
+
+fn read_file_lines(filename: &str) -> Result<Vec<String>, io::Error> {
+    let file = File::open(filename)?;
+    let mut lines: Vec<String> = Vec::new();
+
+    for line in io::BufReader::new(file).lines() {
+        let line_str = line?;
+        lines.push(line_str);
+    }
+
+    Ok(lines)
+}
+
+fn rwc(lines: &Vec<String>) -> WordCount {
+    let mut words_cnt = 0;
+    let mut bytes_cnt = 0;
+    let lines_cnt = lines.len();
+
+    for line in lines {
+        words_cnt += line.split_whitespace().count();
+        bytes_cnt += line.as_bytes().len();
+    }
+
+    WordCount::new(words_cnt, lines_cnt, bytes_cnt)
+}
+
+fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         println!("Too few arguments.");
         process::exit(1);
     }
+
     let filename = &args[1];
-    // Your code here :)
+    let lines = read_file_lines(filename)?;
+    let wordcount = rwc(&lines);
+
+    wordcount.display();
+    Ok(())
 }
