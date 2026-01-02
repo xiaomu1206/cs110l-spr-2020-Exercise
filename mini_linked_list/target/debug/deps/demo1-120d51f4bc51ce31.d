ec45985b6f118e7cc84551342094c09d46a12926
xiaomu <jinyuan_mu@163.com>
2026-01-02 16:02:42 +0800

    delete unuseful something

--- mini_linked_list/target/debug/deps/demo1-120d51f4bc51ce31.d
+++
@@ -1,5 +0,0 @@
-/home/zhuanz/CS110/cs110l-spr-2020-starter-code/demo/demo1/target/debug/deps/demo1-120d51f4bc51ce31.d: src/main.rs
-
-/home/zhuanz/CS110/cs110l-spr-2020-starter-code/demo/demo1/target/debug/deps/libdemo1-120d51f4bc51ce31.rmeta: src/main.rs
-
-src/main.rs:
