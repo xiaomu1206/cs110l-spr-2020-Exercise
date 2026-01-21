556ec50c55df502c569a039f24911148a5b0757b
xiaomu <jinyuan_mu@163.com>
2026-01-21 23:31:48 +0800

    complete week3/milestone4

--- week3/inspect-fds/src/process.rs
+++ week3/inspect-fds/src/process.rs
@@ -1,4 +1,4 @@
-use crate::open_file::OpenFile;
+use crate::open_file::{self, OpenFile};
 #[allow(unused)] // TODO: delete this line for Milestone 3
 use std::fs;
 
@@ -15,7 +15,25 @@ impl Process {
     }
 
     pub fn print(&self) {
-        println!("========== \"{}\" (pid {}, ppid {}) ==========", self.command, self.pid, self.ppid);
+        match self.list_open_files() {
+            None => println!(
+                "Warning: could not inspect file descriptors for this process! \
+                    It might have exited just as we were about to look at its fd table, \
+                    or it might have exited a while ago and is waiting for the parent \
+                to reap it."
+            ),
+            Some(open_files) => {
+                for (fd, file) in open_files {
+                    println!(
+                        "{:<4} {:<15} cursor: {:<4} {}",
+                        fd,
+                        format!("({})", file.access_mode),
+                        file.cursor,
+                        file.colorized_name(),
+                    );
+                }
+            }
+        }
     }
 
     /// This function returns a list of file descriptor numbers for this Process, if that
@@ -43,7 +61,6 @@ impl Process {
     /// This function returns a list of (fdnumber, OpenFile) tuples, if file descriptor
     /// information is available (it returns None otherwise). The information is commonly
     /// unavailable if the process has already exited.
-    #[allow(unused)] // TODO: delete this line for Milestone 4
     pub fn list_open_files(&self) -> Option<Vec<(usize, OpenFile)>> {
         let mut open_files = vec![];
         for fd in self.list_fds()? {
